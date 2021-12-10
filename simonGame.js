8b8c7f7d2265ee09170b067452942bb7c02ed579
RK1PF <graffe.raiha@gmail.com>
2021-12-10 11:50:24 +0100

    simon game debug

--- simonGame.js
+++ simonGame.js
@@ -1,57 +1,121 @@
 const animationTime = 100; //variable servant pour les temps d'animation
-const btns = $(".btn"); // stockage des boutons
+const btns = $(".btn"); // stockage des boutons du game
 let patternToFollow = []; // tableau stockant la pattern en cours
+let nextPattern = [];
 let lvl = 0;
-let gameStarted = false;
+let clickActivated = false;
+let tour = 0;
 $(document).on("keydown", function (e) {
   //event listener sur les touches
-  if (e.key == "0" || e.key == "Insert") {
+  if (e.key == "a" && lvl === 0) {
     startGame();
-  } else {
-    gameOver();
+  }
+  if (e.key == "0") {
+    patternAdd();
   }
 });
+//clique des boutons (fired at every click on any of btns)
 $(btns).on("mousedown", function () {
-  //clique des boutons
+  // assignation du bouton appuyé à la variable btn
+  let btn = $(this).attr("id");
+  comparePattern(patternToFollow, btn)
+  console.log(tour);
+
+  // animation du clique
   $(this).toggleClass("clicked");
   setTimeout(() => {
     $(this).toggleClass("clicked");
   }, animationTime);
+
 });
 function startGame() {
   //fonction regroupant les changements à opérer lors d'un début de partie
-  if (!gameStarted && lvl == 0) {
-    gameStarted = true; //le jeu a commencé et c'est la première partie
-    lvl++;
+  // lvl 1
+  lvl++;
+  animationLvl();
+  // lancement du pattern
+  patternAdd();
+}
+function gameOver() {
+  //fonction regroupant tout les changements à opérer lors d'un game over
+  animationGameOver();
+}
 
-    $("#level-title").text(`level ${lvl}`);
+/*FIXME:debug en cours*/
 
-    $("#level-title").toggleClass("pressed");
-    setTimeout(() => {
-      $("#level-title").toggleClass("pressed");
-    }, animationTime);
+function randomBtn() {
+  let r = btns[Math.floor(Math.random() * btns.length)].getAttribute("id");
+  console.log(r);
+  return r; // retourne l'id d'un bouton aléatoirement
+}
+function patternAdd() {
+  let result = patternToFollow.push(randomBtn());
+  console.log(patternToFollow);
+}
+
+/*FIXME:Debug en cours sur la comparaison*/
+
+function comparePattern(patternToFollow, playersInput) {
+
+  if (patternToFollow[tour] === playersInput) {
+    tour++;
+    console.log("Bon");
+    // play du son attribué au btn
+    playBtnSound(playersInput)
+  } else {
+    gameOver();
+    tour = 0;
+    playBtnSound("wrong");
   }
+  if (tour >= patternToFollow.length) {
+    tour = 0;
+    lvl++;
+    patternAdd();
+    animationLvl();
+  }
+}
+function playBtnSound(btn) {
+  switch (btn) {
+    case "green":
+      (new Audio("./SimonGame/sounds/green.mp3")).play();
+      break;
+    case "red":
+      (new Audio("./SimonGame/sounds/red.mp3")).play();
+      break;
+    case "yellow":
+      (new Audio("./SimonGame/sounds/yellow.mp3")).play();
+      break;
+    case "blue":
+      (new Audio("./SimonGame/sounds/blue.mp3")).play();
+      break;
+    case "wrong":
+      (new Audio("./SimonGame/sounds/wrong.mp3")).play();
+      break;
+    default:
+      break;
+  }
+}
+function animationLvl() {
+  // changement du texte du titre et son css (animation)
+  $("#level-title").text(`level ${lvl}`);
+  $("#level-title").toggleClass("pressed");
+  setTimeout(() => {
+    $("#level-title").toggleClass("pressed");
+    if (lvl <= 1) {
+      // animation du body
+      $("#full").toggleClass("expand");
+      setTimeout(() => {
+        $("#full").toggleClass("expand");
+      }, animationTime * 2);
+    }
+  }, animationTime);
+}
+function animationGameOver() {
   $("body")
     .fadeOut(animationTime, function () {
       $(this).toggleClass("game-over");
     })
     .fadeIn(animationTime, function () {
-      $("#level-title").text(`level ${lvl}`);
+      $("#level-title").text("Game Over");
     });
-}
-function gameOver() {
-  //fonction regroupant tout les changements à opérer lors d'un game over
-  if (gameStarted) {
-    gameStarted = false;
-    $("body")
-      .fadeOut(animationTime, function () {
-        $(this).toggleClass("game-over");
-      })
-      .fadeIn(animationTime, function () {
-        $("#level-title").text("Game Over");
-      });
-  }
-}
-function randomBtn() {
-  return btns[Math.floor(Math.random() * btns.length)]; // tire un numéro aléatoire allant de 0 à nb btns
 }
